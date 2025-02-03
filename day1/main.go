4709935218be477664fa4bfe4bf1dd325f85693e
MohdAadil01 <mohdaadil4240@gmail.com>
2025-02-03 17:29:06 +0530

    variables and looping

--- day1/main.go
+++ day1/main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	// fmt.Println("Hello world")
+
+	var name string = "your name"
+	fmt.Println(name)
+
+	// looping
+
+	// infinite loop
+	// for {
+	// 	println(1)
+	// }
+
+	// for i := 1; i <= 10; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	for i := range 3 {
+		fmt.Println(i)
+	}
 }
